e10d7169cfa4f900ae7e1f05b6f752a1052d3f3f
Md. Zahidul Haque <48868403+zahid88@users.noreply.github.com>
2019-03-24 04:50:17 +0600

    add Listener code

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 /* This is a Javascript file */
+
+var s = function(p){
+  var s1 = "Add text" + p;
+  retrurn s1;
+}
+
+documnent.getElementById("abc").addEventListener("click", function(){
+   // To do code
+});
